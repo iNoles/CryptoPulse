0b76ae2ef109c503a64a68b7613e64a1f3fe92c8
Jonathan Steele <iNoles@users.noreply.github.com>
2025-07-04 21:57:28 -0400

    Initial Commit

---
+++ CryptoPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoPulse
+//
+//  Created by Jonathan Steele on 7/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoPulse/CryptoPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPulseApp.swift
+//  CryptoPulse
+//
+//  Created by Jonathan Steele on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
